861e023239a2f4ae906b29a859d9514ef43f1fd9
Aleksei Semin <alllexsm@gmail.com>
2021-07-23 23:13:46 +0200

    Sorted events

--- dist/index.js
+++ dist/index.js
@@ -4,35 +4,59 @@ fetch("./data/_ref.json")
     .then(response => response.json())
     .then(main);
 
+Object.prototype.myLet = function(f) { f(this); };
+
 async function main(rootLangRef) {
     const langDataset = await fetchLangDataset(rootLangRef.languages);
+    const sortedLangEvents = sortLangEvents(langDataset);
 
     const rootElem = document.getElementById("root");
+    rootElem.appendChild(document.createElement("hr"));
     const timelineRootElem = document.createElement("div");
     timelineRootElem.style.position = "relative";
 
-    let leftOffset = 0;
-    for (const langData of langDataset) {
-        const langDivElem = document.createElement("div");
-        langDivElem.style.position = "relative";
-        langDivElem.style.left = `${leftOffset}px`;
+    const orderedRootElem = document.createElement("div");
+    for (const langEvent of sortedLangEvents) {
+        document.createElement("p").myLet(it => {
+            if (langEvent.init) {
+                it.innerHTML = `<b>${langEvent.language} (${langEvent.date})</b>`;
+            } else {
+                it.innerText = `${langEvent.language} ${langEvent.version} (${langEvent.date})`;
+            }
+            orderedRootElem.appendChild(it);
+        });
+    }
+    timelineRootElem.appendChild(orderedRootElem);
+    rootElem.appendChild(timelineRootElem);
+    rootElem.appendChild(document.createElement("hr"));
+}
 
+function sortLangEvents(langDataset) {
+    const sortedEvents = [];
+    let langIndexCounter = 0;
+    for (const langData of langDataset) {
+        const langIndex = langIndexCounter++;
         for (const langEvent of langData.events) {
-            const langEventElem = document.createElement("p");
-            langEventElem.innerText = `${langData.language} ${langEvent.version} (${langEvent.date})`;
-            langDivElem.appendChild(langEventElem);
+            const langEventExt = {...langEvent, language: langData.language, langIndex};
+            sortedEvents.push(langEventExt);
         }
 
-        const langInitEventElem = document.createElement("p");
-        langInitEventElem.innerHTML = `<b>${langData.language} (${langData.init.date})</b>`;
-        langDivElem.appendChild(langInitEventElem);
-
-        timelineRootElem.appendChild(langDivElem);
-
-        leftOffset += 30;
+        const langInitEventExt = {
+            init: true,
+            date: langData.init.date,
+            language: langData.language,
+            langIndex
+        };
+        sortedEvents.push(langInitEventExt);
     }
 
-    rootElem.appendChild(timelineRootElem);
+    sortedEvents.sort((a, b) => {
+        let c = (+new Date(b.date)) - (+new Date(a.date));
+        if (c !== 0) return c;
+        return b.langIndex - a.langIndex;
+    });
+
+    return sortedEvents;
 }
 
 async function fetchLangDataset(languages) {
